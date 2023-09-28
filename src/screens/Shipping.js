5e95c8f9ddd3989e11cd30d8afcfbc9fd9a5d007
unknown <shivalibhartola@gmail.com>
2023-09-28 11:19:16 +0530

    shipping Screen changes made

--- src/screens/Shipping.js
+++ src/screens/Shipping.js
@@ -1,33 +1,100 @@
 import react from "react";
-import {View,Text,StyleSheet,Platform, StatusBar} from "react-native"
-import {Entypo} from "react-native-vector-icons"
-const Shipping=()=>{
-    return(
-    <View >
-        <StatusBar/>
-        <View style={styles.parent}>
-        <View style={styles.icon_view}>
-            <Entypo  name='chevron-thin-left' size={12} color={'black'} />
+import { View, Text, StyleSheet, Platform, StatusBar } from "react-native"
+import { Entypo } from "react-native-vector-icons"
+import { responsiveHeight as h, responsiveWidth as w } from "react-native-responsive-dimensions";
+const Shipping = () => {
+    return (
+        <View >
+            <StatusBar />
+            <View style={styles.parent}>
+                <View style={styles.icon_view}>
+                    <Entypo name='chevron-thin-left' size={18} color={'black'} />
+                </View>
+                <View style={{ flex: 2, paddingVertical: 15, justifyContent: 'center' }}>
+                    <Text style={{}}>Checkout</Text>
+                </View>
+            </View>
+            <View style={styles.header}>
+                
+            <View style={styles.sub_header}>
+                <View style={{}}>
+                    <Text>Order</Text>
+                </View>
+                <View style={{}}>
+                    <Text>     
+                        <Text>{'\u20B9'}</Text>
+                        <Text>7000</Text>
+                    </Text>
+                
+                </View>
+            </View>
+            <View style={styles.sub_header}>
+                <View style={{}}>
+                    <Text>Shipping</Text>
+                </View>
+                <View style={{}}>
+                    <Text>     
+                        <Text>{'\u20B9'}</Text>
+                        <Text>30</Text>
+                    </Text>
+                
+                </View>
+            </View>
+            <View style={styles.sub_header1}>
+                <View style={{}}>
+                    <Text>Total</Text>
+                </View>
+                <View style={{}}>
+                    <Text>     
+                        <Text>{'\u20B9'}</Text>
+                        <Text>7030</Text>
+                    </Text>
+                
+                </View>
+            </View>
+            <View>
+                <Text>Payment</Text>
+            </View>
+            <View>
+                
+            </View>
         </View>
-        <View style={{}}>
-        <Text style={{fontSize:10}}>Checkout</Text>
         </View>
-        </View>
-    </View>
+
     )
 }
 export default Shipping
-const styles=StyleSheet.create({
-    parent:{
-    flexDirection:'row',
+const styles = StyleSheet.create({
+    parent: {
+        flexDirection: 'row',
+
+    },
+
+    icon_view: {
+        // alignItems:'center',
+        // backgroundColor:'red',
+        justifyContent: "center",
+        // alignItems:"center",
+        flex: 1.4,
+        paddingLeft: 10
+    },
+    sub_header: {
+        paddingHorizontal: w(3),
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        // backgroundColor:'blue',
     
     },
-   
-    icon_view:{
-        // alignItems:'center',
-        backgroundColor:'red',
-        justifyContent:"center",
-        // alignItems:"center",
-        
+    sub_header1: {
+        paddingHorizontal: w(3),
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        // backgroundColor:'blue',
+        borderBottomWidth:StyleSheet.hairlineWidth,
+        paddingBottom:10 
+    },
+    header:{
+        marginHorizontal:10,
     }
+
 })
